d01703f0073c9ffb603b0aad0a94dd682a7ce62b
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-14 10:25:48 +0100

    Update main method to allow a choice between calculation methods

--- src/hugo/simplearithmetic/Main.java
+++ src/hugo/simplearithmetic/Main.java
@@ -3,6 +3,22 @@ package hugo.simplearithmetic;
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.main();
+        UserInput userInput = new UserInput();
+        boolean isActive = true;
+
+        while (isActive) {
+            int sumType = userInput.getIntInput("Select calculation input type:\n1. Guided input\n2. Manual input\n3. Quit");
+
+            switch (sumType) {
+                case 1:
+                    calculator.stepByStep();
+                    break;
+                case 2:
+                    calculator.totalInput();;
+                    break;
+                default:
+                    isActive = false;
+            }
+        }
     }
 }
