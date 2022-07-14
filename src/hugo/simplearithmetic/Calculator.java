d611f5716469e269458f32921cc322d24451363d
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-14 10:25:06 +0100

    Change name of main method to match method of calculation

--- src/hugo/simplearithmetic/Calculator.java
+++ src/hugo/simplearithmetic/Calculator.java
@@ -2,59 +2,40 @@ package hugo.simplearithmetic;
 
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class Calculator {
-
-    Scanner scanner = new Scanner(System.in);
-
     public void printMessage(String message) {
         System.out.println(message);
     }
 
-    public int getIntInput(String message) {
-        printMessage(message);
-        return scanner.nextInt();
-    }
-
-    public String getStringInput(String message) {
-        printMessage(message);
-        return scanner.next();
-    }
-
-    public void main() {
+    public void stepByStep() {
 
         UserInput userInput = new UserInput();
-        boolean isActive = true;
 
-        while (isActive) {
-            int num1 = userInput.getIntInput("Enter first number:");
-            int num2 = userInput.getIntInput("Enter second number:");
-            String operator = userInput.getStringInput("Enter operator:");
+        int num1 = userInput.getIntInput("Enter first number:");
+        int num2 = userInput.getIntInput("Enter second number:");
+        String operator = userInput.getStringInput("Enter operator:");
 
-            switch (operator) {
-                case "+":
-                    Addition addition = new Addition();
-                    addition.performAddition(num1, num2);
-                    break;
-                case "-":
-                    Subtraction subtraction = new Subtraction();
-                    subtraction.performSubtraction(num1, num2);
-                    break;
-                case "*":
-                    Multiplication multiplication = new Multiplication();
-                    multiplication.performMultiplication(num1, num2);
-                    break;
-                case "/":
-                    Division division = new Division();
-                    division.performDivision(num1, num2);
-                    break;
-                default:
-                    System.out.println("Operator not recognised");
-            }
-
-            int input = userInput.getIntInput("Would you like to perform another calculation?\n1. Yes\n2. No");
-            if (input == 2) {
-                isActive = false;
-            }
+        switch (operator) {
+            case "+":
+                Addition addition = new Addition();
+                addition.performAddition(num1, num2);
+                break;
+            case "-":
+                Subtraction subtraction = new Subtraction();
+                subtraction.performSubtraction(num1, num2);
+                break;
+            case "*":
+                Multiplication multiplication = new Multiplication();
+                multiplication.performMultiplication(num1, num2);
+                break;
+            case "/":
+                Division division = new Division();
+                division.performDivision(num1, num2);
+                break;
+            default:
+                System.out.println("Operator not recognised");
         }
     }
 }
