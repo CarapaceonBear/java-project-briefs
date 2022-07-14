33bf30846f6f134b57ac75e678834b524f9d8c6d
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-14 10:25:20 +0100

    Add totalInput method of calculation

--- src/hugo/simplearithmetic/Calculator.java
+++ src/hugo/simplearithmetic/Calculator.java
@@ -38,4 +38,36 @@ public class Calculator {
                 System.out.println("Operator not recognised");
         }
     }
+
+    public void totalInput() {
+        UserInput userInput = new UserInput();
+
+        String sum = userInput.getSumInput("Enter sum:");
+        String[] separatedSum = sum.split(" ", 3);
+
+        int num1 = parseInt(separatedSum[0]);
+        String operator = separatedSum[1];
+        int num2 = parseInt(separatedSum[2]);
+
+        switch (operator) {
+            case "+":
+                Addition addition = new Addition();
+                addition.performAddition(num1, num2);
+                break;
+            case "-":
+                Subtraction subtraction = new Subtraction();
+                subtraction.performSubtraction(num1, num2);
+                break;
+            case "*":
+                Multiplication multiplication = new Multiplication();
+                multiplication.performMultiplication(num1, num2);
+                break;
+            case "/":
+                Division division = new Division();
+                division.performDivision(num1, num2);
+                break;
+            default:
+                System.out.println("Operator not recognised");
+        }
+    }
 }
